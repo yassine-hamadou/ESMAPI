616bd65eb98063a1be3721caaed0397274f7421d
kofi-k <kofivincent026@gmail.com>
2023-05-26 18:16:02 +0000

    activity details added

---
+++ Data/ProActivityDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ServiceManagerApi.Data;
+
+public partial class ProActivityDetail
+{
+    public int Id { get; set; }
+
+    public int? ActivityId { get; set; }
+
+    public string? Code { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? TenantId { get; set; }
+
+    public virtual ProductionActivity? Activity { get; set; }
+}
