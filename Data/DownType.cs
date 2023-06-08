039185fadbfae89f86d1ab8ac385541c2f58e1ae
Yassine H.M <68260654+yassine-hamadou@users.noreply.github.com>
2023-06-08 17:20:52 +0000

    SCaffold

---
+++ Data/DownType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ServiceManagerApi.Data;
+
+public partial class DownType
+{
+    public string? DownType1 { get; set; }
+
+    public int Id { get; set; }
+}
