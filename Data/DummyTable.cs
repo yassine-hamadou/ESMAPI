a94f758a58f0105f407085cd4e29910291ee988e
Yassine H.M <68260654+yassine-hamadou@users.noreply.github.com>
2023-08-28 09:51:53 +0000

    Dummy Table

---
+++ Data/DummyTable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ServiceManagerApi.Data;
+
+public partial class DummyTable
+{
+  public string? Name { get; set; }
+
+  public int Id { get; set; }
+}
